2f89187950d90c81653d630b0ddb6e19cb3ba046
Wuqibor <77870838+Wuqibor@users.noreply.github.com>
2021-02-13 22:22:30 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,14 +2,14 @@
 
 #下载资源
 #  樱花FRPC
-curl -L -H "Cache-Control: no-cache" -o /usr/local/yhfrpc https://getfrp.sh/d/frpc_linux_amd64
+curl -L -H "Cache-Control: no-cache" -o /tmp/yhfrpc https://getfrp.sh/d/frpc_linux_amd64
 #  Screen
-curl -L -H "Cache-Control: no-cache" -o /usr/local/screen https://raw.githubusercontent.com/Wuqibor/FRP-Herkou/main/screen
+curl -L -H "Cache-Control: no-cache" -o /tmp/screen https://raw.githubusercontent.com/Wuqibor/FRP-Herkou/main/screen
 #  FRPS
-curl -L -H "Cache-Control: no-cache" -o /usr/local/frps $FRPURL
+curl -L -H "Cache-Control: no-cache" -o /tmp/frps $FRPURL
 
 #写入樱花FRPC配置文件
-cat << EOF > /usr/local/frps.ini
+cat << EOF > /tmp/frps.ini
 [common]
 bind_port = $FRPSERVERPORT
 token = $FRPSERVERTOKEN
@@ -18,18 +18,18 @@ vhost_https_port = $PORT
 EOF
 
 #修正权限
-chmod +x /usr/local/yhfrpc
-chmod +x /usr/local/frps
-chmod +x /usr/local/screen
+chmod +x /tmp/yhfrpc
+chmod +x /tmp/frps
+chmod +x /tmp/screen
 
 #创建樱花FRP使用的后台Screen
 screen_name=$FRPCSCREEN
-/usr/local/screen -dmS $FRPCSCREEN
+/tmp/screen -dmS $FRPCSCREEN
 
 #使用用户提供的参数连接樱花FRP
-cmd=$"/usr/local/yhfrpc -f $UID:$RID"
-/usr/local/screen -x -S $screen_name -p 0 -X stuff $cmd
-/usr/local/screen -x -S $screen_name -p 0 -X stuff '\n'
+cmd=$"/tmp/yhfrpc -f $UID:$RID"
+/tmp/screen -x -S $screen_name -p 0 -X stuff $cmd
+/tmp/screen -x -S $screen_name -p 0 -X stuff '\n'
 
 #运行FRP服务端
-/usr/local/frps -c /usr/local/frps.ini
+/tmp/frps -c /tmp/frps.ini
